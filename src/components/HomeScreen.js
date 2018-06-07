e450ff39daa7be6a3f97e99dea558c58d46f49a7
omeiregman <omeiregodsman@gmail.com>
2018-06-07 13:04:30 +0100

    removed flight tab

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -11,7 +11,7 @@ class HomeScreen extends Component {
     return (
       <div>
         <Banner/>
-        <FlightHotelTab/>
+        {/* <FlightHotelTab/> */}
         <TripDashboard/>
       </div>
 
