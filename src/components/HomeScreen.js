c3447426c314501f4963d6c26ecb0754b9d9f144
godbmg <omeiregodsman@telesoftas.com>
2020-03-26 01:21:43 +0200

    add eslint config and config file

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import TripDashboard from "./HomeTripsComponent/TripDashboard";
 import "./FlightHotelsComponent/css/flight-hotel.css";
 import "../css/index-style.css";
-import FlightHotelTab from "./FlightHotelsComponent/FlightHotelTab";
 import Banner from "./HeaderBannerComponent/Banner";
 
-class HomeScreen extends Component {
-  render() {
-    return (
-      <div>
-        <Banner />
-        <TripDashboard />
-      </div>
-    );
-  }
-}
+const HomeScreen = () => {
+  return (
+    <div>
+      <Banner />
+      <TripDashboard />
+    </div>
+  );
+};
 
 export default HomeScreen;
